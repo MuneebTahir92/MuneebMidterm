00fc1c0bac793b774e3872ce72bb79e43bc352f0
Muneeb <muneeb@Muneebs-MacBook-Pro.local>
2021-03-17 13:39:45 -0600

    map

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,11 @@
 package datastructure;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -9,6 +15,33 @@ public class UseMap {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 *
 		 */
+
+		List<String> toyotaModels = new ArrayList<String>();
+		toyotaModels.add("camry");
+		toyotaModels.add("corolla");
+		toyotaModels.add("sienna");
+
+		List<String> hondaModels = new ArrayList<String>();
+		hondaModels.add("accord");
+		hondaModels.add("civic");
+		hondaModels.add("odyssey");
+
+		Map<String, List<String>> cars = new HashMap<String, List<String>>();
+		cars.put("Toyota Models", toyotaModels);
+		cars.put("Honda Models", hondaModels);
+
+		ListIterator<String, List<String>> it = cars.listIterator();
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
+
+		for(Map.Entry<String, List<String>> entry: cars.entrySet()){
+			System.out.println(entry.getKey() + " ----> " + entry.getValue());
+		}
+
+
+
+
 
 	}
 
