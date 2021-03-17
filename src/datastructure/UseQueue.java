a64c2b602b354793e7d077c0c94c3f1d8b5bb0db
Muneeb <muneeb@Muneebs-MacBook-Pro.local>
2021-03-17 13:54:45 -0600

    queue

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,6 +1,8 @@
 package datastructure;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class UseQueue {
@@ -12,7 +14,28 @@ public class UseQueue {
 		 * 
 		 */
 		Queue<String> queue = new LinkedList<>();
-		queue.poll();
+		queue.add("muneeb");
+		queue.add("ali");
+		queue.add("chetan");
+		queue.add("uddin");
+		queue.add("ahmed");
+		queue.add("barry");
+		queue.add("monkey");
+
+		System.out.println(queue.peek());
+		System.out.println(queue.poll());
+		System.out.println(queue.remove());
+		System.out.println(queue.remove());
+		System.out.println(queue.peek());
+
+		for(String names:queue){
+			System.out.println(names);
+		}
+
+		Iterator it = queue.iterator();
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
 
 	}
 
