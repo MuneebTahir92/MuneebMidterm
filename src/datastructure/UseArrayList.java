a414c53944387a118c9ee5d19c482a60bcbd06d3
Muneeb <muneeb@Muneebs-MacBook-Pro.local>
2021-03-17 13:12:35 -0600

    arraylist

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -10,7 +14,30 @@ public class UseArrayList {
 		 * 
 		 */
 
-		
+		List<String> list = new ArrayList<String>();
+
+		list.add("muneeb");
+		list.add("ali");
+		list.add("chetan");
+		list.add("uddin");
+		list.add("ahmed");
+		list.add("barry");
+		list.add("monkey");
+		list.remove("monkey");
+
+		Iterator<String> it = list.listIterator();
+
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
+
+		for(String names:list){
+			System.out.println(names);
+		}
+
+
+
+
 	
 
 	}
