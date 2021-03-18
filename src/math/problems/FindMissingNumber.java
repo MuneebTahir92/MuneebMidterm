d2ad61c56aef71f167b354ed5ec559d5b28c3be2
Muneeb <muneeb@Muneebs-MacBook-Pro.local>
2021-03-17 19:42:56 -0600

    findmissingnumber

--- src/math/problems/FindMissingNumber.java
+++ src/math/problems/FindMissingNumber.java
@@ -1,9 +1,22 @@
 package math.problems;
 
+import java.util.Arrays;
+
 /**
  * Created by mrahman on 04/22/17.
  */
 public class FindMissingNumber {
+
+    public static int findMissing(int[]array, int n){
+        int i, total;
+
+        total = (n + 1) * (n + 2) / 2;
+
+        for(i=0; i<n; i++)
+            total -= array[i];
+            return total;
+
+    }
 
     public static void main(String[] args) {
         /*
@@ -12,6 +25,11 @@ public class FindMissingNumber {
          * Write java code to find the missing number from the array. Write static helper method to find it.
          */
          int [] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+
+        int miss = findMissing(array,9);
+        System.out.println(miss);
+
+
 
     }
 }
