7bdbac806297837937f3f9a322b1f895df74179e
Muneeb <muneeb@Muneebs-MacBook-Pro.local>
2021-03-18 12:02:23 -0600

    pattern

--- src/math/problems/Pattern.java
+++ src/math/problems/Pattern.java
@@ -7,6 +7,22 @@ public class Pattern {
 		 * 100,99,98,97,96,95,94,93,92,91,90,88,86,84,82,80,78,76,74,72,70,67,64,61,58,55,52,49,46,43,40,36,32............
 		 *
 		 */
+
+		for(int a = 100; a >= 90; a--){
+			System.out.print(a + " ");
+		}
+
+		for(int b = 88; b >= 70; b -= 2){
+			System.out.print(b + " ");
+		}
+
+		for(int c = 67; c >= 40; c -= 3){
+			System.out.print(c + " ");
+		}
+
+		for(int d = 36; d >= 0; d -= 4){
+			System.out.print(d + " ");
+		}
 		
 
 
